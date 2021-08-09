d4f4ea11569b2de7a5375ce709f39aff836c5c91
fabio98p <parisifabio98@gmail.com>
2021-08-09 16:49:09 +0200

    lezione 7/8, tubi e hitbox

--- index.py
+++ index.py
@@ -3,74 +3,121 @@ import random
 
 pygame.init()
 background = pygame.image.load("./img/background.jpg")
-bird = pygame.image.load("./img/bird.png")
+bird = pygame.image.load("./img/bird.jpg")
 base = pygame.image.load("./img/base.svg")
 gameOver = pygame.image.load("./img/gameOver.png")
-pipeUp = pygame.image.load("./img/pipe.png")
-pipeDown = pygame.transform.flip(pipeUp, False, True)
+pipeDown = pygame.image.load("./img/pipe.png")
+pipeUp = pygame.transform.flip(pipeDown, False, True)
 
 windows = pygame.display.set_mode((1920, 1080))
-FPS = 50
+FPS = 10000
 vel_wolrd = 3
 repeateBase = 573.75
 
 
+class pipe_class:
+    def __init__(self):
+        self.x = 1920
+        self.y = random.randint(100, 400)
+
+    def avanzamento(self):
+        self.x -= vel_wolrd
+        windows.blit(pipeDown, (self.x, self.y+500))
+        windows.blit(pipeUp, (self.x, self.y-500))
+
+    def triggher(self, bird, birdx, birdy):
+        tolleranza = 0
+        bird_dx = birdx+bird.get_width()-tolleranza
+        bird_sx = birdx+tolleranza
+        bird_up = birdy+tolleranza
+        bird_down = birdy+bird.get_height()-tolleranza
+
+        pipe_dx = self.x + pipeDown.get_width()
+        pipe_sx = self.x
+        pipe_up = self.y
+        pipe_down = self.y+500
+
+        if  bird_sx < pipe_dx and bird_dx > pipe_sx:
+            if bird_down > pipe_down or bird_up < pipe_up:
+                looser()
+
 # dichiarazione funzione riguardante lo status iniziale del ucceollo
-def inizializza() :
-	global birdx,birdy, bird_vely, basex
-	birdx, birdy = 200, 200
-	bird_vely = 0
-	basex = 0
+
+
+def inizializza():
+    global birdx, birdy, bird_vely
+    global basex
+    global pipe
+    birdx, birdy = 200, 200
+    bird_vely = 0
+    basex = 0
+    pipe = []
+    pipe.append(pipe_class())
 
 # dichiarazione funzione riguardante l'inserimento delle immagini all'interno del gioco
-def draw_object() : 
-	windows.blit(background, (0,0))
-	windows.blit(bird, (birdx,birdy))
-	# rendere il il pavimento in movimento ma essendo corto ho dovuto ripeterlo in loop
-	basexLoop = basex -repeateBase
-	for pippo in range(5) :
-		basexLoop += repeateBase
-		windows.blit(base, (basexLoop,700))
-	basexLoop = 0
+
+
+def draw_object():
+    windows.blit(background, (0, 0))
+    for t in pipe:
+        t.avanzamento()
+    windows.blit(bird, (birdx, birdy))
+    # rendere il il pavimento in movimento ma essendo corto ho dovuto ripeterlo in loop
+    basexLoop = basex - repeateBase
+    for pippo in range(5):
+        basexLoop += repeateBase
+        windows.blit(base, (basexLoop, 700))
+    basexLoop = 0
 
 # dichiarazione funzione per il refresch di ogni fotogrammo
-def refresh() : 
-	pygame.display.update()
-	pygame.time.Clock().tick(FPS)
+
+
+def refresh():
+    pygame.display.update()
+    pygame.time.Clock().tick(FPS)
 
 # funzione che viene chiamata quando perdi
-def looser() :
-	# stampa game over
-	windows.blit(gameOver, (500, 100))
-	refresh()
-	reset = False
-	# while che blocca l'intero gioco finche non si usa uno dei comandi assegnati
-	while not reset:
-		for event in pygame.event.get() :
-			if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) :
-				reset = True
-				inizializza()
-			if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-				pygame.quit()
+
+
+def looser():
+    # stampa game over
+    windows.blit(gameOver, (500, 100))
+    refresh()
+    reset = False
+    # while che blocca l'intero gioco finche non si usa uno dei comandi assegnati
+    while not reset:
+        for event in pygame.event.get():
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+                reset = True
+                inizializza()
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.quit()
 
 
 inizializza()
 
 while True:
-	#avvenimenti indipendenti dal giocatore
-	bird_vely += 1
-	if basex < -repeateBase: basex = 0
-	birdy += bird_vely
-	basex -= vel_wolrd
-	# comandi per comandare roba
-	for event in pygame.event.get() :
-		if (event.type == pygame.KEYDOWN and event.key == pygame.K_UP) :
-			bird_vely = -10
-		if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-			pygame.quit()
-	#quando perdi
-	if birdy > 500 : looser()
-	# modifica la posizione degli elementi a sachermo
-	draw_object()
-	# passa al frame successivo
-	refresh()
+    # avvenimenti indipendenti dal giocatore
+    bird_vely += 1
+    if basex < -repeateBase:
+        basex = 0
+    birdy += bird_vely
+    basex -= vel_wolrd
+    # comandi per comandare roba
+    for event in pygame.event.get():
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_UP):
+            bird_vely = -10
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            pygame.quit()
+
+    if pipe[-1].x < 1300:
+        pipe.append(pipe_class())
+    for t in pipe:
+        t.triggher(bird, birdx, birdy)
+    # quando perdi
+    if birdy > 900:
+        looser()
+    # modifica la posizione degli elementi a sachermo
+    draw_object()
+    # passa al frame successivo
+    refresh()
